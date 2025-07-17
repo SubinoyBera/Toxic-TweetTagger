4254159e4386f728dc3bae169b998aa9ee81bc1b
Subinoy Bera <subinoyberadgp@gmail.com>
2025-07-17 23:47:58 +0530

    data preprocessing logic added

---
+++ src/components/data_preprocessing.py
@@ -0,0 +1,128 @@
+import sys
+import string
+from pathlib import Path
+from src.logger.logging import logging
+from src.exception.app_exception import AppException
+from src.utils.common import create_directory
+from src.config.configuration import AppConfiguration
+
+import pandas as pd
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+
+class HelperFunctions:
+    def __init__(self) -> None:
+        """
+        Initializes an instance of the HelperFunctions class.
+
+        This class contains methods for various text preprocessing tasks such as
+        lowercasing, removing punctuations, removing stopwords, and lemmatization.
+        """
+        pass
+    
+    def lower_case(self, text):
+        """Converts the given text to lowercase."""
+        return text.tolower()
+    
+    def remove_punctuations(self, text):
+        """Removes all punctuation marks from the given text."""
+        exclude = string.punctuation
+        return text.translate(str.maketrans("", "", exclude))
+    
+    def remove_stopwords(self, text):
+        """Removes all English stopwords from the given text."""
+        stop_words = set(stopwords.words('english'))
+        text = [word for word in text.split() if word not in stop_words]
+        return " ".join(text)
+    
+    def lemmatization(self, text):
+        """Converts all words in the given text to their base form using WordNet lemmatization."""
+        lemmatizer = WordNetLemmatizer()
+        text_words = text.split()
+        text = [lemmatizer.lemmatize(word) for word in text_words]
+        return " ".join(text)
+
+
+class DataPreprocessing:
+    def __init__(self, config = AppConfiguration() ):
+        """
+        Initializes the DataPreprocessing object by creating a data preprocessing configuration.
+
+        Raises:
+            AppException: If error occurs during creation of data preprocessing configuration
+        """
+
+        try:
+            self.data_preprocessing_config = config.data_preprocessing_config()
+
+        except Exception as e:
+            logging.error(f"Failed to create data preprocessing configuration: {e}", exc_info=True)
+            raise AppException(e, sys)
+
+
+    def preprocess(self, df):
+        """
+        Performs different data processing steps on the given dataframe
+            Lowercasing the content,
+            Removing punctuations from the content,
+            Removing english stopwords, and Lemmatizing the content.
+        
+        Saves the preprocessed data in csv file after performing data preprocessing.
+
+        Raises:
+            AppException: If error occurs during data preprocessing
+        """
+
+        # Sampling from dataframe to create a smaller working dataset
+        subset_hate_df = df[df['Label']==1].sample(n=60000, random_state=42)
+        subset_nornal_df = df[df['Label']==0].sample(n=60000, random_state=42)
+
+        subset_df = pd.concat([subset_hate_df, subset_nornal_df])
+
+        fn = HelperFunctions()
+        # Preprocessing steps
+        try:
+            logging.info("Performing lowercasing")
+            df['Content'] = subset_df['Content'].apply(fn.lower_case)
+
+            logging.info("Removing punctuations")
+            subset_df['Content'] = subset_df['Content'].apply(fn.remove_punctuations)
+
+            logging.info("Removing stopwords")
+            subset_df['Content'] = subset_df['Content'].apply(fn.remove_stopwords)
+
+            print("\n Performing lemmatization")
+            subset_df['Content'] = subset_df['Content'].apply(fn.lemmatization)
+
+            logging.info("Finished preprocessing successfully")
+            processed_data_dir = self.data_preprocessing_config.processed_data_dir
+            create_directory(processed_data_dir)
+            subset_df.to_csv(Path(processed_data_dir, 'clean_data.csv'), index=False)
+            logging.info(f"Data successfully saved at {processed_data_dir}")
+        
+        except Exception as e:
+            logging.error(f"Data preprocessing failed: {e}", exc_info=True)
+            raise AppException(e, sys)
+    
+    def initiate_preprocessing(self):
+        """
+        Initiates the data preprocessing process by calling the preprocess method.
+        """
+        try:
+            logging.info(f"{'='*20}Data Preprocessing{'='*20}")
+            data_path = self.data_preprocessing_config.ingested_dataset_path
+            if not data_path:
+                logging.error("No data path found")
+                return
+            df = pd.read_csv(data_path, encoding='utf-8')
+            obj = DataPreprocessing()
+            obj.preprocess(df)
+            logging.info(f"{'='*20}Data Preprocessing Completed Successfully{'='*20} \n\n")
+
+        except Exception as e:
+            logging.error(f"Error in Data Preprocessing process: {e}", exc_info=True)
+            raise AppException(e, sys)
