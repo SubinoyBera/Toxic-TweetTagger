c67150a66840dd6b508baee3fcdc810df7b9c2a5
Subinoy Bera <subinoyberadgp@gmail.com>
2025-07-18 20:50:50 +0530

    feature engineering and model training implementation added

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,10 +1,12 @@
+# Project pipeline configuration 
 import sys
 from pathlib import Path
 from src.logger.logging import logging
 from src.constants.constants import *
 from src.utils.common import read_yaml, create_directory
 from src.exception.app_exception import AppException
-from src.entity.config_entity import (DataIngestionConfig, DataPreprocessingConfig)
+from src.entity.config_entity import (DataIngestionConfig, DataPreprocessingConfig,
+                                      FeatureEngineeringConfig, ModelTrainingConfig)
 
 class AppConfiguration:
     def __init__(self, 
@@ -41,14 +43,14 @@ class AppConfiguration:
             raw_data_path = Path(ingestion_root, raw_dir)
             ingested_data_path = Path(ingestion_root, ingested_data_dir)
 
-            ingestion_configuration = DataIngestionConfig(
+            self.ingestion_configuration = DataIngestionConfig(
                 raw_data_dir = raw_data_path,
-                ingested_dir = ingested_data_path,
+                ingested_data_dir = ingested_data_path,
                 data_download_url = download_url
             )
         
             logging.info("Data Ingestion Configuration creation successfull")
-            return ingestion_configuration
+            return self.ingestion_configuration
         
         except Exception as e:
             logging.error(f"Error while creating Data Ingestion Configuration: {e}", exc_info=True)
@@ -62,24 +64,74 @@ class AppConfiguration:
         """
         try:
             preprocessing_config = self.config.data_preprocessing
-            ingestion_config = self.config.ingestion_config
 
-            preprocessing_root = preprocessing_config.root_dir
-            processed_data_dir = preprocessing_config.processed_data_dir
+            preprocessing_root = Path(preprocessing_config.root_dir)
             dataset = preprocessing_config.dataset
 
             create_directory(preprocessing_root)
 
-            processed_data_dir_path = Path(preprocessing_root, processed_data_dir)
-            dataset_path = Path(ingestion_config.ingested_data, dataset)
+            dataset_path = Path(self.ingestion_configuration.ingested_data_dir, dataset)
 
-            preprocessing_configuration = DataPreprocessingConfig(
-                processed_data_dir = processed_data_dir_path,
+            self.preprocessing_configuration = DataPreprocessingConfig(
+                preprocessed_data_dir = preprocessing_root,
                 ingested_dataset_path = dataset_path
             )
 
-            return preprocessing_configuration
+            return self.preprocessing_configuration
         
         except Exception as e:
             logging.error(f"Error while creating Data Preprocessing Configuration: {e}", exc_info=True)
+            raise AppException(e, sys)
+        
+
+    def feature_engineering_config(self) -> FeatureEngineeringConfig:
+        """
+        Creates the configuration for Feature Engineering.
+        Returns: FeatureEngineeringConfig object
+        """
+        try:
+            feature_eng_config = self.config.feature_engineering
+
+            feature_eng_root_dir = Path(feature_eng_config.root_dir)
+            models_root_dir = Path(feature_eng_config.models_dir)
+
+            create_directory(feature_eng_root_dir)
+            create_directory(models_root_dir)
+
+            preprocessed_data = feature_eng_config.preprocessed_dataset
+            preprocessed_data_path = Path(self.preprocessing_configuration.preprocessed_data_dir, preprocessed_data)
+
+            self.feature_engineering_configuration = FeatureEngineeringConfig(
+                models_dir = models_root_dir,
+                preprocessed_data_path = preprocessed_data_path,
+                train_test_data_path = feature_eng_root_dir
+            )
+
+            return self.feature_engineering_configuration
+        
+        except Exception as e:
+            logging.error(f"Error while creating Feature Engineering Configuration: {e}", exc_info=True)
+            raise AppException(e, sys)
+
+
+    def model_training_config(self) -> ModelTrainingConfig:
+        """
+        Creates the configuration for Model Training.
+        Returns: ModelTrainingConfig object
+        """
+        try:
+            training_config = self.config.model_training
+
+            models_dir_path = Path(training_config.model_dir)
+            train_data_path = self.feature_engineering_configuration.train_test_data_path
+
+            training_configuration = ModelTrainingConfig(
+                train_data_path = train_data_path,
+                models_dir = models_dir_path
+            )
+        
+            return training_configuration
+
+        except Exception as e:
+            logging.error(f"Error while creating Model Training Configuration: {e}", exc_info=True)
             raise AppException(e, sys)
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -5,9 +5,20 @@ from pathlib import Path
 class DataIngestionConfig:
     data_download_url: str
     raw_data_dir: Path
-    ingested_dir: Path
+    ingested_data_dir: Path
 
 @dataclass(frozen=True)
 class DataPreprocessingConfig:
     ingested_dataset_path: Path
-    processed_data_dir: Path
+    preprocessed_data_dir: Path
+
+@dataclass(frozen=True)
+class FeatureEngineeringConfig:
+    models_dir: Path
+    preprocessed_data_path: Path
+    train_test_data_path: Path
+
+@dataclass(frozen=True)
+class ModelTrainingConfig:
+    train_data_path: Path
+    models_dir: Path
