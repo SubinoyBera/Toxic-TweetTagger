bbd3976bb0cdb281fb08ecea712e96e157c04a92
Subinoy Bera <subinoyberadgp@gmail.com>
2025-07-30 12:56:52 +0530

    added model testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,21 @@
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+# This file contains tests for the FastAPI application endpoints.
 import pytest
 from fastapi import status
 from app.app import app
 from fastapi.testclient import TestClient
 
-client =TestClient(app)
+client = TestClient(app)
+
 
 @pytest.mark.asyncio
 async def test_health_check():
+    """
+    Test the health check endpoint.
+
+    This test sends a GET request to the /health endpoint and verifies that the response is 200 OK, 
+    and that the response contains the message, model name, and model version in its JSON payload.
+    
+    """
     response = client.get("/health")
     assert response.status_code == status.HTTP_200_OK
     json_data = response.json()
@@ -17,8 +23,16 @@ async def test_health_check():
     assert "model" in json_data
     assert "version" in json_data
 
+
 @pytest.mark.asyncio
 async def test_home_page():
+    """
+    Test the home page endpoint.
+
+    This test sends a GET request to the / endpoint and verifies that the response is 200 OK, 
+    and that the response contains the HTML content type in its headers.
+    
+    """
     response = client.get("/")
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
@@ -26,9 +40,19 @@ async def test_home_page():
 
 @pytest.mark.asyncio
 async def test_predict_page():
+    """
+    Test the predict page endpoint.
+
+    This test sends a POST request to the /predict endpoint with a valid tweet and verifies 
+    that the response is 200 OK, and that the response contains the HTML content type in its 
+    headers, and that the response contains either :
+    ["highly toxic", "toxic", "likely to be toxic", or "looks safe"]
+    
+    """
     response = client.post("/predict", data={"tweet": "I love that movie! It was great"})
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
+    # Check if the response contains expected phrases
     assert any(
         phrase in response.text
         for phrase in [
@@ -39,12 +63,23 @@ async def test_predict_page():
         ]
     )
 
-
+  
+@pytest.mark.asyncio
 async def test_api_endpoint():
+    """
+    Test the /api endpoint.
+
+    This test sends a POST request to the /api endpoint with a sample tweet and verifies 
+    that the response status is 200 OK.
+    Checks if the response contains both 'response' and 'metadata' keys and validates that 
+    the 'confidence' value in the 'response' is a float between 0 and 1.
+    
+    """
     response = client.post("/api", params={"tweet": "I love that movie! It was great"})
     assert response.status_code == 200
     data = response.json()
     
+    # Validate the response structure
     assert "response" in data
     assert "metadata" in data
     assert "confidence" in data["response"]
