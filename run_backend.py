cfbb861540720fb23e95f1b777b9baa4212836a6
Subinoy Bera <subinoyberadgp@gmail.com>
2026-03-06 12:36:31 +0530

    updated app run scripts

--- run_backend.py
+++ run_backend.py
@@ -1,13 +1,9 @@
-# Script for starting the FastAPI application
+# Script for starting the FastAPI backend application server.
 
 import uvicorn
-import gunicorn
 from src.core.logger import logging
-
-#if __name__ == "__main__":
-#    gunicorn src.run_app:app -k uvicorn.workers.UvicornWorker --reload
 
 
 if __name__ == "__main__":
     logging.info("Starting app server...")
-    uvicorn.run("src.app.main:app", host="0.0.0.0", port=8000, workers=2)
+    uvicorn.run("src.app.main:app", host="0.0.0.0", port=8000)
